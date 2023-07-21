0bf532bd4fb42636ab14bee5650676bd3bf26e9b
Julio7-7-7 <julio.toledo2030@gmail.com>
2023-07-21 00:37:01 -0400

    repositioning

---
+++ primeraPágina/apuntes.rb
@@ -0,0 +1,9 @@
+1)¿Que es el anidamiento en html?
+En HTML, el anidamiento se refiere a la colocación de elementos HTML dentro de otros elementos HTML. 
+Cada elemento en HTML puede contener otros elementos dentro de él, creando una estructura jerárquica.
+El elemento que contiene a otros elementos se conoce como elemento padre, mientras que los elementos 
+contenidos se denominan elementos hijos.
+
+*Todos los elementos img deben tener un atributo alt. El texto del atributo alt es utilizado por lectores 
+de pantalla para mejora la accesibilidad y es mostrado en caso de que la imagen falle en cargar.
+
